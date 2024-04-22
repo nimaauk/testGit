d42d0555bf1a4a709a8a820c1cf054200f180f84
Nima Abedpour <nima.abedpour@uni-koeln.de>
2024-04-22 14:43:31 +0200

    First Commit

---
+++ testGit.py
@@ -0,0 +1,11 @@
+# print happy birthday function
+def happy_birthday(name):
+    print("Happy Birthday to you")
+    print("Happy Birthday to you")
+    print(f"Happy Birthday dear {name}")
+    print("Happy Birthday to you")
+
+
+happy_birthday("Nima")
+
+
